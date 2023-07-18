9af809eadc6a5405b4dc13aea65cac70bca3de16
Denise <deniseabecian@gmail.com>
2023-07-18 13:11:07 -0300

    feat(liteflix-challenge): componente Loader

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,6 +1,20 @@
 import Image from 'next/image'
+import Loader from '../Loader'
 
-export default function Button({ type, disabled = false, className, onClick, image, text }) {
+/**
+ * Button.
+ * Puede mostrar un Loader como una imagen y/o un texto.
+ */
+
+export default function Button({ type, disabled = false, className, onClick, image, text, isLoading }) {
+  if (isLoading) {
+    return (
+      <button type={type} disabled={disabled} className={className} onClick={onClick}>
+        <Loader />
+      </button>
+    )
+  }
+
   return (
     <button type={type} disabled={disabled} className={className} onClick={onClick}>
       {image && (
